6916239dd882662270883834e1bf52ada1143e2f
tomozaidem <tomozaidem@gmail.com>
2019-11-04 01:30:13 +0800

    Updates

---
+++ precache-manifest.332f86b2977e741d1eeb42cf680409e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccd5230fa7ce14961a0360d3ffbae42",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f0269d367d71a0c57f68",
+    "url": "/monsters-rolodex/static/css/main.6f46e0f4.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "f0269d367d71a0c57f68",
+    "url": "/monsters-rolodex/static/js/main.33b21664.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
